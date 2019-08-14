3d8e15ca41796c7f2bd4efe6ba1a6d398a1ba68f
Russell Seamer <russell.seamer@reyno.co.uk>
2019-08-14 09:36:35 +0100

    Update hello-world.ts

--- packages/my-plugin/src/elements/hello-world.ts
+++ packages/my-plugin/src/elements/hello-world.ts
@@ -1,12 +1,6 @@
-import { bindable, inlineView } from 'aurelia-framework';
+import { bindable, inlineView, useView } from 'aurelia-framework';
 
-/*
-@inlineView(`
-  <template>
-    <h3 class="hello-world">Hello world \${message}</h3>
-  </template>
-`)
-*/
+@useView(PLATFORM.moduleName("./hello-world.html"))
 export class HelloWorld {
   @bindable public message: string = '';
 }
